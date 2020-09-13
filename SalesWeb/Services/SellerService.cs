e41928e7207abef303cd95a1979c3bd6e428304e
williamaiadias <williamaiadias@hotmail.com>
2020-09-13 10:43:31 -0300

    SellerService and basic FindAl

---
+++ SalesWeb/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿using SalesWeb.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWeb.Models;
+using SalesWeb.Data;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SalesWeb.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebContext _context;
+
+        public SellerService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
